4829ec341b0c0e6c5fdac07c374dc0b51c13575b
kimminjae <kmjnet0515@gmail.com>
2025-04-09 17:44:33 +0900

    캘린더추가, 요약 사진 반전 추가

--- src/app/frontend/projectSummary.tsx
+++ src/app/frontend/projectSummary.tsx
@@ -8,28 +8,31 @@ const Summary = () => {
         {/* 개별 블록 */}
         <div className="summary-top p-4 text-center rounded-lg">
           <div className="flex items-center gap-2 ml-1">
-            <Image src="/checkLogo.jpg" alt="check" width={60} height={60} />
+            <Image className="summaryImages" src="/checkLogo.jpg" alt="check" width={60} height={60} />
             <h1 className="font-bold">0개 완료함</h1>
           </div>
         </div>
         <div className="summary-top p-4 text-center rounded-lg">
           <div className="flex items-center gap-2 ml-1">
-            <Image src="/updateLogo.jpg" alt="check" width={60} height={60} />
+            <Image className="summaryImages" src="/updateLogo.jpg" alt="check" width={60} height={60} />
             <h1 className="font-bold">0개 업데이트함</h1>
           </div>
         </div>
         <div className="summary-top p-4 text-center rounded-lg">
           <div className="flex items-center gap-2 ml-1">
-            <Image src="/madeLogo.jpg" alt="check" width={60} height={60} />
+            <Image className="summaryImages" src="/madeLogo.jpg" alt="check" width={60} height={60} />
             <h1 className="font-bold">0개 만듦</h1>
           </div>
         </div>
         <div className="summary-top p-4 text-center rounded-lg">
           <div className="flex items-center gap-2 ml-1">
-            <Image src="/deadlineLogo.jpg" alt="check" width={60} height={60} />
+            <Image className="summaryImages" src="/deadlineLogo.jpg" alt="check" width={60} height={60} />
             <h1 className="font-bold">0개 마감 예정</h1>
           </div>
         </div>
+      </div>
+      <div>
+
       </div>
     </div>
   );
