6f981227169d1cfa815ae97b12c3defe3625df54
kimminjae <kmjnet0515@gmail.com>
2025-05-15 15:01:39 +0900

    aa

--- backendChat/test.py
+++ backendChat/test.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 import spacy
 from openai import OpenAI
 client = OpenAI(
-  api_key="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"
+  api_key=""
 )
 app = Flask(__name__)
 
@@ -39,5 +39,3 @@ def analyze():
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5002)
-#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
-#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
